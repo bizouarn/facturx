ba5cf2a6e06175ea09e7b4c7a516c08c32c11c7f
Aymeric BIZOUARN <a.bizouarn@herakles.com>
2026-01-23 09:24:47 +0100

    Fixes TradeAllowanceCharge type in LineTradeSettlement

--- Securibox.FacturX/SpecificationModels/Extended/TradeAllowanceChargeExtended.cs
+++ Securibox.FacturX/SpecificationModels/Extended/TradeAllowanceChargeExtended.cs
@@ -1,16 +1,8 @@
 ﻿namespace Securibox.FacturX.SpecificationModels.Extended
 {
-    public class TradeAllowanceChargeExtended
+    public class TradeAllowanceChargeExtended : BasicWL.TradeAllowanceCharge
     {
-        public BasicWL.IndicatorType ChargeIndicator { get; set; }
         public string SequenceNumeric { get; set; }
-        public decimal CalculationPercent { get; set; }
-        public Minimum.Amount BasisAmount { get; set; }
         public Basic.Quantity BasisQuantity { get; set; }
-        public Minimum.Amount ActualAmount { get; set; }
-        public string ReasonCode { get; set; }
-        public string Reason { get; set; }
-        public TradeTaxExtended CategoryTradeTax { get; set; }
-
     }
 }
